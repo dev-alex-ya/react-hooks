e8d7139181b2e29b03ae6aa378fd7c2680e889bf
AlexYa <ya.alexandr.86@gmail.com>
2020-06-04 18:57:37 +0300

    polug in Search text input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {AlertContext} from '../context/alert/alertContext';
 
-
-
 export const Search = () => {
+  const [value, setValue] = useState('');
   const {show} = useContext(AlertContext);
+
   const onSubmit = (event) => {
-    if(event.key === 'Enter') {
-      show('This is alert!')
+    // если нажат не enter, то ничего не делаем
+    if(event.key !== 'Enter') {
+      return;
+    }
+
+    if (value.trim()) {
+      console.log(value.trim())
+    } else {
+      show('Введите данные пользователя!')
     }
   }
 
@@ -17,6 +24,8 @@ export const Search = () => {
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        value={value}
+        onChange={event => setValue(event.target.value)}
         onKeyPress={onSubmit}
       />
     </div>
